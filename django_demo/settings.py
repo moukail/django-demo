dffd5d98562a95170774a1fc7ba8ea8ddd6928fb
¨Ismail <i.moukafih@gmail.com>
2025-04-11 12:13:48 +0200

    static files

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -122,11 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    #BASE_DIR / "blog/static",    
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'blog.CustomUser'
